7697261bdb74cdcf95e776e24a42907af674d31d
aticu <15schnic@gmail.com>
2017-04-02 15:48:51 +0200

    started documenting better

--- src/veos.rs
+++ src/veos.rs
@@ -14,10 +14,12 @@ mod io;
 mod arch;
 mod boot;
 
+///Defines the name of the operating system.
 static OS_NAME: &str = "VeOS";
 
+///The main entry point for the operating system. This gets called by the loader.
 #[no_mangle]
-pub extern fn main(magic_number: u32, information_structure_address: usize) {
+pub extern fn main(magic_number: u32, information_structure_address: usize) -> ! {
     boot::init(magic_number, information_structure_address);
     io::init();
     println!("Booted {} using {}...", OS_NAME, boot::get_bootloader_name());
@@ -27,10 +29,13 @@ pub extern fn main(magic_number: u32, information_structure_address: usize) {
     }
 }
 
+//TODO: add doc
 #[lang = "eh_personality"]
 extern fn eh_personality() {
+    unimplemnted!();
 }
 
+///This function gets called when the operating system panics. It aims to provide as much information as possible.
 #[lang = "panic_fmt"]
 #[no_mangle]
 pub extern fn panic_fmt(fmt: core::fmt::Arguments, file: &'static str, line: u32) -> ! {
